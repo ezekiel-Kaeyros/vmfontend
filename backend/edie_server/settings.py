e7dbbf77aace8908a3311497fd031c1c2db31671
ezekiel-kaeyros <ezekiel.dombissi@kaeyros-analytics.com>
2024-05-07 16:27:01 +0100

    add

--- backend/edie_server/settings.py
+++ backend/edie_server/settings.py
@@ -160,7 +160,7 @@ DATABASES = {
         "NAME": os.environ.get("POSTGRES_NAME"),
         "USER": os.environ.get("POSTGRES_USER"),
         "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
-        "HOST": "172.17.0.2",
+        "HOST": "db",
         "PORT": 5432,
     }
 }
